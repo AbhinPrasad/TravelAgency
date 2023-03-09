4999a664dd07219f4ec50dc4d22f873827ae43f0
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-03-09 17:36:44 +0530

    initialized react app using vite

---
+++ client/src/constants/images.js
@@ -0,0 +1,29 @@
+import circle1 from "../assets/circle 1.png";
+import circle2 from "../assets/circle 2.png";
+import circle3 from "../assets/circle 3.png";
+import dot1 from "../assets/dot 1.png";
+import ellipse8 from "../assets/Ellipse 8.png";
+import ellipse9 from "../assets/Ellipse 9.png";
+import frame10 from "../assets/Frame 10.png";
+import location from "../assets/Location.png";
+import map from "../assets/map.png";
+import plane from "../assets/plane.png";
+import rectangle8 from "../assets/Rectangle 8.png";
+import rectangle9 from "../assets/Rectangle 9.png";
+import compass from "../assets/Vector.png";
+
+export default {
+	circle1,
+	circle2,
+	circle3,
+	dot1,
+	ellipse8,
+	ellipse9,
+	frame10,
+	location,
+	map,
+	plane,
+	rectangle8,
+	rectangle9,
+	compass
+};
